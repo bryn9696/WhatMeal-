19b92f80792bd8c15a255710623ea9bc36ce2c4d
bryn9696 <bryn2009@live.co.uk>
2022-04-22 16:25:57 +0100

    recipes.rb split into seperate methods and refactored

--- lib/recipes.rb
+++ lib/recipes.rb
@@ -8,27 +8,23 @@ class Recipes
     self.ham_sandwhich(ingredients_list)
 
     if @options.length >= 1
-      return @options.join(', ')
+      p @options.join(', ')
     else
-      return "Have you got anything else?!"
+      p "Have you got anything else?!"
     end
   end
 
   def cheese_toasty(ingredients_list)
     cheese_toasty = ['cheese', 'bread']
     cheese_toasty.each do |i|
-      if ingredients_list.include?(i) == true
-        @options << "cheese toasty"
-      end
+      ingredients_list.include?(i) == true ? @options << "Cheese Toasty" : @options
     end
   end
 
   def ham_sandwhich(ingredients_list)
     ham_sandwhich = ['ham', 'bread']
     ham_sandwhich.each do |i|
-      if ingredients_list.include?(i) == true
-        @options << "ham_sandwhich"
-      end
+      ingredients_list.include?(i) == true ? @options << "Ham Sandwhich" : @options
     end
   end
 end
--- spec/recipes_spec.rb
+++ spec/recipes_spec.rb
@@ -3,12 +3,17 @@ require './lib/recipes'
 describe Recipes do
   it 'returns recipe matching one ingredient (when given first)' do
     recipe = Recipes.new
-    expect(recipe.choices(["cheese", "pork"])).to eq('cheese toasty')
+    expect(recipe.choices(["cheese", "pork"])).to eq('Cheese Toasty')
   end
 
   it 'returns recipe matching one ingredient (when given second)' do
     recipe = Recipes.new
-    expect(recipe.choices(["pork", "cheese"])).to eq('cheese toasty')
+    expect(recipe.choices(["pork", "cheese"])).to eq('Cheese Toasty')
+  end
+
+  it 'returns recipe matching one ingredient (when given second)' do
+    recipe = Recipes.new
+    expect(recipe.choices(["ham", "egg"])).to eq('Ham Sandwhich')
   end
 
   it 'returns error message when ingredients dont match any recipes' do
@@ -18,11 +23,11 @@ describe Recipes do
 
   it 'returns all available recipes' do
     recipe = Recipes.new
-    expect(recipe.choices(["ham", "cheese"])).to eq("cheese toasty, ham_sandwhich")
+    expect(recipe.choices(["ham", "cheese"])).to eq("Cheese Toasty, Ham Sandwhich")
   end
 
   it 'returns all available recipes' do
     recipe = Recipes.new
-    expect(recipe.choices(["egg", "ham", "cheese"])).to eq("cheese toasty, ham_sandwhich")
+    expect(recipe.choices(["egg", "ham", "cheese"])).to eq("Cheese Toasty, Ham Sandwhich")
   end
 end
