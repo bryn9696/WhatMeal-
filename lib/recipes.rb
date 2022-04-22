892a62e09213e5d6048b8550510dd7f0ba754ab9
bryn9696 <bryn2009@live.co.uk>
2022-04-22 15:42:22 +0100

    returns all available recipes, needs refactoring and further tests

--- lib/recipes.rb
+++ lib/recipes.rb
@@ -3,11 +3,26 @@ class Recipes
   attr_reader :ingredients_list
 
   def choices(ingredients_list)
+    cheese_toasty = ['cheese', 'bread']
+    ham_sandwhich = ['ham', 'bread']
+    options = []
     p ingredients_list
-    if ingredients_list.include?("cheese") == true
-      return 'cheese toasty'
+    cheese_toasty.each do |i|
+      if ingredients_list.include?(i) == true
+        options << "cheese toasty"
+      end
+    end
+
+    ham_sandwhich.each do |i|
+      if ingredients_list.include?(i) == true
+        options << "ham_sandwhich"
+      end
+    end
+
+    if options.length >= 1
+      return options.join(', ')
     else
-      return 'Have you got anything else?!'
+      return "Have you got anything else?!"
     end
   end
 end
--- spec/recipes_spec.rb
+++ spec/recipes_spec.rb
@@ -3,16 +3,21 @@ require './lib/recipes'
 describe Recipes do
   it 'returns recipe matching one ingredient (when given first)' do
     recipe = Recipes.new
-    expect(recipe.choices(["cheese", "ham"])).to eq('cheese toasty')
+    expect(recipe.choices(["cheese", "pork"])).to eq('cheese toasty')
   end
 
   it 'returns recipe matching one ingredient (when given second)' do
     recipe = Recipes.new
-    expect(recipe.choices(["ham", "cheese"])).to eq('cheese toasty')
+    expect(recipe.choices(["pork", "cheese"])).to eq('cheese toasty')
   end
 
   it 'returns error message when ingredients dont match any recipes' do
     recipe = Recipes.new
-    expect(recipe.choices(["beef", "ham"])).to eq('Have you got anything else?!')
+    expect(recipe.choices(["beef", "pork"])).to eq('Have you got anything else?!')
+  end
+
+  it 'returns all available recipes' do
+    recipe = Recipes.new
+    expect(recipe.choices(["ham", "cheese"])).to eq("cheese toasty, ham_sandwhich")
   end
 end
