5bb6597ee3b13b0c76b7bb8741c73a26ab53bfc3
bryn9696 <bryn2009@live.co.uk>
2022-04-22 16:05:27 +0100

    split resipes into individual methods, tests pass

--- lib/recipes.rb
+++ lib/recipes.rb
@@ -3,25 +3,32 @@ class Recipes
   attr_reader :ingredients_list
 
   def choices(ingredients_list)
-    cheese_toasty = ['cheese', 'bread']
-    ham_sandwhich = ['ham', 'bread']
-    options = []
-    cheese_toasty.each do |i|
-      if ingredients_list.include?(i) == true
-        options << "cheese toasty"
-      end
-    end
+    @options = []
+    self.cheese_toasty(ingredients_list)
+    self.ham_sandwhich(ingredients_list)
 
-    ham_sandwhich.each do |i|
-      if ingredients_list.include?(i) == true
-        options << "ham_sandwhich"
-      end
-    end
-
-    if options.length >= 1
-      return options.join(', ')
+    if @options.length >= 1
+      return @options.join(', ')
     else
       return "Have you got anything else?!"
+    end
+  end
+
+  def cheese_toasty(ingredients_list)
+    cheese_toasty = ['cheese', 'bread']
+    cheese_toasty.each do |i|
+      if ingredients_list.include?(i) == true
+        @options << "cheese toasty"
+      end
+    end
+  end
+
+  def ham_sandwhich(ingredients_list)
+    ham_sandwhich = ['ham', 'bread']
+    ham_sandwhich.each do |i|
+      if ingredients_list.include?(i) == true
+        @options << "ham_sandwhich"
+      end
     end
   end
 end
