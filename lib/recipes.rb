4e7cd66a2723a0ce3f780aff4ff1d01f334b4f70
bryn9696 <bryn2009@live.co.uk>
2022-04-25 15:26:21 +0100

    refactored recipes code

--- lib/recipes.rb
+++ lib/recipes.rb
@@ -2,25 +2,26 @@
 class Recipes
   attr_reader :ingredients_list
 
-  def choices(ingredients_list)
+  def initialize
     @options = []
+    @cheese_toasty = ['cheese', 'bread']
+    @ham_sandwhich = ['ham', 'bread']
+  end
+
+  def choices(ingredients_list)
     self.cheese_toasty(ingredients_list)
     self.ham_sandwhich(ingredients_list)
-
     @options.length >= 1 ? @options.uniq.join(', ') : "Have you got anything else?!"
- 
   end
 
   def cheese_toasty(ingredients_list)
-    cheese_toasty = ['cheese', 'bread']
-    cheese_toasty.each do |i|
+    @cheese_toasty.each do |i|
       ingredients_list.include?(i) == true ? @options << "Cheese Toasty" : @options
     end
   end
 
   def ham_sandwhich(ingredients_list)
-    ham_sandwhich = ['ham', 'bread']
-    ham_sandwhich.each do |i|
+    @ham_sandwhich.each do |i|
       ingredients_list.include?(i) == true ? @options << "Ham Sandwhich" : @options
     end
   end
