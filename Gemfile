fa48f267037493cd00221e5997baf139bfe65bc0
bryn9696 <bryn2009@live.co.uk>
2022-05-04 12:00:01 +0100

    set up to turn into web app

---
+++ Gemfile
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+ruby '3.0.2'
+
+gem 'pg'
+gem 'rack'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'sinatra-flash', '~> 0.3.0'
+gem 'webrick'
+
+group :test do
+  gem 'capybara'
+  gem 'rspec'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
+
+group :development, :test do
+  gem 'rubocop', '1.20'
+end
---
+++ app.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+require 'sinatra/base'
+require 'sinatra/flash'
+require 'sinatra/reloader'
+# require './database_connection_setup'
+# require './lib/bookings'
+require_relative './lib/ingredients'
+# require './lib/user'
+# require_relative './lib/update'
+# require './lib/images'
+
+
+# App class
+class Meal_Choice < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  enable :sessions
+  register Sinatra::Flash
+
+  get '/' do
+    erb :index
+  end
+
+end
