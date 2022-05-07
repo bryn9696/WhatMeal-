c1cbb7826dde41e3ef0804e4d8fa3c1b66ebf2af
bryn9696 <bryn2009@live.co.uk>
2022-05-07 16:25:35 +0100

    model and controller need to interact

--- app.rb
+++ app.rb
@@ -31,9 +31,19 @@ class Meal_Choice < Sinatra::Base
     @ingredients_list.push(params[:ing3])
     @ingredients_list.push(params[:ing4])
     @ingredients_list.push(params[:ing5])
+    
     p @ingredients_list
+    p (params[:ing1])
+    # Recipes.choices(@ingredients_list)
+    # @d = Ingredients.dictionary(@ingredients_list)
+    # @c = Ingredients.choice(@ingredients_list)
+    
     erb :user_input
     # @ingredients = Ingredients.ingredients
+  end
+
+  get '/meal' do
+    p @ingredients_list
   end
 
 end
--- lib/ingredients.rb
+++ lib/ingredients.rb
@@ -3,10 +3,9 @@ require_relative 'recipes.rb'
 class Ingredients
   attr_accessor :ingredients_list
 
-  def initialize
-    @recipes = Recipes.new
-    @ingredients_list = []
-  end
+  # def initialize
+  #   @recipes = Recipes.new
+  # end
 
   def self.ingredients(ingredients_list)
     # total = 1
@@ -15,10 +14,10 @@ class Ingredients
     #   @ingredients_list << input.to_s.downcase.capitalize
     #   total += 1
     # end
-    @ingredients_list = @ingredients_list.uniq.sort
+    @ingredients_list = ingredients_list.uniq.sort
   end
 
-  def dictionary
+  def self.dictionary(ingredients_list)
     spell_check = @recipes.ingredients.uniq.sort   
     spell_check.each do |ing|
       i = 0
@@ -36,24 +35,10 @@ class Ingredients
       end
       i += 1
     end
-    # @ingredients_list = @ingredients_list.uniq.sort
-
-    # n = spell_check.length
-    # @ingredients_list.each do |ing|
-    #   ind = 0
-    #   ing_length = 0
-    #   spell_check.each do |og|
-    #     if og != ing  
-    #       ing_length += 1
-    #       p "#{@ingredients_list.delete(ing)} #{ing_length}"
-    #     end
-    #   end
-    #   ind += 1
-    # end
     p @ingredients_list.uniq.sort
   end
 
-  def choice
-    @recipes.choices(@ingredients_list)
+  def choice(ingredients_list)
+    @recipes.choices(ingredients_list)
   end
 end
